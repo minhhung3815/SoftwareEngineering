258853461dc1db2bed9bf254a3ab5e48019a38b6
Phạm Hoàng Hải <hai.pham.hoang@hcmut.edu.vn>
2021-11-11 22:02:10 +0700

    style: Fix style by black

--- POS System/app.py
+++ POS System/app.py
@@ -1,26 +1,25 @@
-from flask import Flask,render_template, url_for, redirect, request, flash, abort
-import sqlite3
-from flask_login._compat import unicode
 import datetime
-from forms import RegisterForm, LoginForm
+import sqlite3
+
+from flask import (Flask, abort, flash, redirect, render_template, request,
+                   url_for)
 from flask_bcrypt import Bcrypt
-from flask_login import LoginManager, login_user, login_required, UserMixin, logout_user, current_user
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import (LoginManager, UserMixin, current_user, login_required,
+                         login_user, logout_user)
+from flask_login._compat import unicode
+from werkzeug.security import check_password_hash, generate_password_hash
 
+from forms import LoginForm, RegisterForm
 
-
-
-app = Flask(__name__) 
+app = Flask(__name__)
 app.secret_key = ['832790179812']
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message = 'Please login to access this page!'
 login_manager.login_message_category = "warning"
-db = sqlite3.connect('test.db',check_same_thread=False)
+db = sqlite3.connect('test.db', check_same_thread=False)
 db.close()
-
-
 
 
 class User(UserMixin):
@@ -59,15 +58,14 @@ def load_user(user_id):
         return User(int(lu[0]), lu[1], lu[2], lu[3])
 
 
-@app.route('/', methods = ["POST", "GET"])
+@app.route('/', methods=["POST", "GET"])
 def index():
 
     if request.method == "POST":
         number = request.form["quantity"]
         id = request.form["id"]
 
-
-        return redirect(url_for("choose", number = number, id = id))
+        return redirect(url_for("choose", number=number, id=id))
 
     else:
         conn = sqlite3.connect('test.db')
@@ -75,10 +73,14 @@ def index():
         curs = conn.execute("SELECT * FROM FOOD")
         food = curs.fetchall()
 
-        curs = conn.execute("select * from CART where ID = (?)", (current_user.get_id(), ))
+        curs = conn.execute(
+            "select * from CART where ID = (?)", (current_user.get_id(),)
+        )
         bill = curs.fetchall()
 
-        curs = conn.execute("select count(*) from CART where ID = (?)", (current_user.get_id(), ))
+        curs = conn.execute(
+            "select count(*) from CART where ID = (?)", (current_user.get_id(),)
+        )
         number = curs.fetchone()
 
         total = 0
@@ -86,7 +88,10 @@ def index():
             total += item[2] * item[5]
 
         conn.close()
-        return render_template("index.html", food = food, bill = bill, number = number, total = total)
+        return render_template(
+            "index.html", food=food, bill=bill, number=number, total=total
+        )
+
 
 @app.route('/choose/<number>/<id>/')
 def choose(number, id):
@@ -96,27 +101,37 @@ def choose(number, id):
     curs = conn.execute("select * from FOOD where ID = (?)", (id,))
     food = curs.fetchone()
 
-
-    curs = conn.execute("select * from CART where ID = (?) and food_name = (?)", (current_user.get_id(), food[0]))
+    curs = conn.execute(
+        "select * from CART where ID = (?) and food_name = (?)",
+        (current_user.get_id(), food[0]),
+    )
     data = curs.fetchone()
     if not data:
-        curs = conn.execute("insert into CART(ID,food_name,amount,path,food_id,price)values(?,?,?,?,?,?)", (current_user.get_id(), food[0], number, food[3], food[5], food[1]))
+        curs = conn.execute(
+            "insert into CART(ID,food_name,amount,path,food_id,price)values(?,?,?,?,?,?)",
+            (current_user.get_id(), food[0], number, food[3], food[5], food[1]),
+        )
         conn.commit()
         conn.close()
     else:
-        curs = conn.execute("update CART set amount = (?) where ID = (?) and food_name = (?)", (data[2] + int(number), current_user.get_id(), food[0]))
+        curs = conn.execute(
+            "update CART set amount = (?) where ID = (?) and food_name = (?)",
+            (data[2] + int(number), current_user.get_id(), food[0]),
+        )
         conn.commit()
         conn.close()
     return redirect("/")
 
-@app.route('/remove/<id>', methods = ["GET", "POST"])
+
+@app.route('/remove/<id>', methods=["GET", "POST"])
 def remove(id):
     conn = sqlite3.connect('test.db')
     conn.execute("delete from CART where food_id = (?)", (id,))
     conn.commit()
     return redirect("/")
 
-@app.route('/updatehistory/', methods = ["POST", "GET"])
+
+@app.route('/updatehistory/', methods=["POST", "GET"])
 @login_required
 def updatehistory():
     id = current_user.get_id()
@@ -129,23 +144,29 @@ def updatehistory():
     for item in data:
         total += item[2] * item[5]
 
-    cur.execute("insert into history(id_user, time, total)values(?,?,?)", (id, datetime.date.today() ,total))
+    cur.execute(
+        "insert into history(id_user, time, total)values(?,?,?)",
+        (id, datetime.date.today(), total),
+    )
     con.commit()
 
     cur.execute("select MAX(id) from history")
     max_his = cur.fetchone()
 
     for item in data:
-        cur.execute("insert into history_detail(id,food,amount,id_user)values(?,?,?,?)",
-                        (max_his[0], item[1], item[2], current_user.get_id()))
+        cur.execute(
+            "insert into history_detail(id,food,amount,id_user)values(?,?,?,?)",
+            (max_his[0], item[1], item[2], current_user.get_id()),
+        )
         con.commit()
-    
+
     cur.execute("delete from CART where id = (?)", (id,))
     con.commit()
     con.close()
     return redirect('/')
 
-@app.route('/history/', methods = ["POST", "GET"])
+
+@app.route('/history/', methods=["POST", "GET"])
 @login_required
 def history():
     con = sqlite3.connect("test.db")
@@ -153,13 +174,12 @@ def history():
     cur.execute("select * from history where id_user = (?)", (current_user.get_id(),))
     his = list(cur.fetchall())
 
-    
-    cur.execute("select * from history_detail where id_user = (?)", (current_user.get_id(), ))
+    cur.execute(
+        "select * from history_detail where id_user = (?)", (current_user.get_id(),)
+    )
     his_detail = list(cur.fetchall())
-    return render_template("history.html", his = his, his_detail = his_detail)
-    
-    
-    
+    return render_template("history.html", his=his, his_detail=his_detail)
+
 
 @app.route('/removeall/')
 def removeall():
@@ -168,7 +188,8 @@ def removeall():
     conn.commit()
     return redirect("/")
 
-@app.route('/profile', methods = ["GET", "POST"])
+
+@app.route('/profile', methods=["GET", "POST"])
 @login_required
 def profile():
     if request.method == "POST":
@@ -177,8 +198,7 @@ def profile():
         age = request.form["AGE"]
         con = sqlite3.connect("test.db")
         cur = con.cursor()
-        cur.execute("insert into PROFILE(id, name, age)values(?,?,?)",
-                    (id, name, age))
+        cur.execute("insert into PROFILE(id, name, age)values(?,?,?)", (id, name, age))
         con.commit()
         con.close()
         return redirect(url_for('profile'))
@@ -187,7 +207,7 @@ def profile():
         cur = con.cursor()
         cur.execute("select * from PROFILE where id=?", (current_user.get_id(),))
         data = cur.fetchone()
-        return render_template('profile.html',data=data)
+        return render_template('profile.html', data=data)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -210,8 +230,10 @@ def register():
             flash("Email are already exists", "danger")
             return redirect(url_for('register'))
         else:
-            cur.execute("insert into user(username,email,password)values(?,?,?)",
-                        (username, email, password))
+            cur.execute(
+                "insert into user(username,email,password)values(?,?,?)",
+                (username, email, password),
+            )
         con.commit()
         con.close()
         return redirect(url_for('login'))
@@ -227,18 +249,24 @@ def login():
     if form.validate_on_submit():
         con = sqlite3.connect("test.db")
         cur = con.cursor()
-        cur.execute("select * from user where username=?",
-                    [form.username.data])
+        cur.execute("select * from user where username=?", [form.username.data])
         data = cur.fetchall()
         if data:
             user = load_user(data[0][0])
-            if form.username.data == user.username and user.verify_password(form.password.data):
+            if form.username.data == user.username and user.verify_password(
+                form.password.data
+            ):
                 login_user(user)
                 return redirect('/')
             else:
-                flash(f'Username or Password mismatch. Please try again!', category='danger')
-        else: 
-            flash(f'Username or Password mismatch. Please try again!', category='danger')
+                flash(
+                    f'Username or Password mismatch. Please try again!',
+                    category='danger',
+                )
+        else:
+            flash(
+                f'Username or Password mismatch. Please try again!', category='danger'
+            )
     return render_template('login.html', form=form)
 
 
@@ -246,92 +274,110 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for("index"))
-    
 
-@app.route('/rice/', methods = ["GET", "POST"])
+
+@app.route('/rice/', methods=["GET", "POST"])
 def rice():
-    
+
     params = ["Rice"]
     conn = sqlite3.connect('test.db')
     curs = conn.cursor()
     curs = conn.execute("SELECT * FROM FOOD WHERE Type = (?)", params)
     food = curs.fetchall()
-    curs = conn.execute("select * from CART where ID = (?)", (current_user.get_id(), ))
+    curs = conn.execute("select * from CART where ID = (?)", (current_user.get_id(),))
     bill = curs.fetchall()
 
-    curs = conn.execute("select count(*) from CART where ID = (?)", (current_user.get_id(), ))
+    curs = conn.execute(
+        "select count(*) from CART where ID = (?)", (current_user.get_id(),)
+    )
     number = curs.fetchone()
     conn.close()
     total = 0
     for item in bill:
         total += item[2] * item[5]
-    return render_template("index.html", total = total,food = food, bill = bill, number = number)
+    return render_template(
+        "index.html", total=total, food=food, bill=bill, number=number
+    )
 
-@app.route('/chicken/', methods = ["GET", "POST"])
+
+@app.route('/chicken/', methods=["GET", "POST"])
 def chicken():
-    
+
     params = ["Chicken"]
     conn = sqlite3.connect('test.db')
     curs = conn.cursor()
     curs = conn.execute("SELECT * FROM FOOD WHERE Type = (?)", params)
     food = curs.fetchall()
-    curs = conn.execute("select * from CART where ID = (?)", (current_user.get_id(), ))
+    curs = conn.execute("select * from CART where ID = (?)", (current_user.get_id(),))
     bill = curs.fetchall()
 
-    curs = conn.execute("select count(*) from CART where ID = (?)", (current_user.get_id(), ))
+    curs = conn.execute(
+        "select count(*) from CART where ID = (?)", (current_user.get_id(),)
+    )
     number = curs.fetchone()
     conn.close()
 
     total = 0
     for item in bill:
-        total += item[2] * item[5]    
-    return render_template("index.html", total = total, food = food, bill = bill, number = number)
+        total += item[2] * item[5]
+    return render_template(
+        "index.html", total=total, food=food, bill=bill, number=number
+    )
 
-@app.route('/snack/', methods = ["GET", "POST"])
+
+@app.route('/snack/', methods=["GET", "POST"])
 def snack():
-    
+
     params = ["Snack"]
     conn = sqlite3.connect('test.db')
     curs = conn.cursor()
     curs = conn.execute("SELECT * FROM FOOD WHERE Type = (?)", params)
     food = curs.fetchall()
 
-    curs = conn.execute("select * from CART where ID = (?)", (current_user.get_id(), ))
+    curs = conn.execute("select * from CART where ID = (?)", (current_user.get_id(),))
     bill = curs.fetchall()
 
-    curs = conn.execute("select count(*) from CART where ID = (?)", (current_user.get_id(), ))
+    curs = conn.execute(
+        "select count(*) from CART where ID = (?)", (current_user.get_id(),)
+    )
     number = curs.fetchone()
     conn.close()
     total = 0
     for item in bill:
         total += item[2] * item[5]
-    return render_template("index.html", total = total, food = food, bill = bill, number = number)
+    return render_template(
+        "index.html", total=total, food=food, bill=bill, number=number
+    )
 
-@app.route('/drink/', methods = ["GET", "POST"])
+
+@app.route('/drink/', methods=["GET", "POST"])
 def drink():
-    
+
     params = ["Drink"]
     conn = sqlite3.connect('test.db')
     curs = conn.cursor()
     curs = conn.execute("SELECT * FROM FOOD WHERE Type = (?)", params)
     food = curs.fetchall()
-    curs = conn.execute("select * from CART where ID = (?)", (current_user.get_id(), ))
+    curs = conn.execute("select * from CART where ID = (?)", (current_user.get_id(),))
     bill = curs.fetchall()
 
-    curs = conn.execute("select count(*) from CART where ID = (?)", (current_user.get_id(), ))
+    curs = conn.execute(
+        "select count(*) from CART where ID = (?)", (current_user.get_id(),)
+    )
     number = curs.fetchone()
 
     conn.close()
     total = 0
     for item in bill:
         total += item[2] * item[5]
-    return render_template("index.html", total = total, food = food, bill = bill, number = number)
-
+    return render_template(
+        "index.html", total=total, food=food, bill=bill, number=number
+    )
 
 
 if __name__ == '__main__':
-   app.run(debug = True)
+    app.run(debug=True)
 
 
-#Set-ExecutionPolicy Unrestricted -Scope Process
-#env\Scripts\activate
+# Set-ExecutionPolicy Unrestricted -Scope Process
+# env\Scripts\activate
