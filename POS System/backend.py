64b426ad73ded5c605508e8065f4797a90515b07
Phạm Hoàng Hải <hai.pham.hoang@hcmut.edu.vn>
2021-11-15 08:39:09 +0700

    feat: add backend API for add cart item

--- POS System/backend.py
+++ POS System/backend.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-from flask import Blueprint
+from flask import Blueprint, abort, request
 from flask_login import current_user, login_required
 
 backend = Blueprint("backend", __name__)
@@ -22,6 +22,40 @@ def remove_cart_item(item_id: int):
     con = sqlite3.connect("test.db")
     user_id = current_user.get_id()
     con.execute("DELETE FROM cart WHERE id = ? AND food_id = ?", (user_id, item_id))
+    con.commit()
+    con.close()
+    return "", 204
+
+
+@backend.route("/cart", methods=["POST"])
+@login_required
+def add_cart_item():
+    data = request.get_json()
+    if data is None:
+        abort(400)
+
+    con = sqlite3.connect("test.db")
+    con.row_factory = sqlite3.Row
+    cur = con.execute("SELECT * FROM food WHERE id = ?", (data["item_id"],))
+    food = cur.fetchone()
+
+    params = {
+        "user_id": int(current_user.get_id()),
+        "food_name": food["name"],
+        "quantity": data["quantity"],
+        "path": food["path"],
+        "food_id": data["item_id"],
+        "price": food["price"],
+    }
+    con.execute(
+        """
+        INSERT INTO cart(id, food_name, amount, path, food_id, price)
+        VALUES (:user_id, :food_name, :quantity, :path, :food_id, :price)
+        ON CONFLICT (id, food_id) DO
+        UPDATE SET amount = amount + :quantity
+    """,
+        params,
+    )
     con.commit()
     con.close()
     return "", 204
